63622c1db46dc46d0a369ebf513d1f9af1f86295
ahh <ahh666@qq.com>
2024-05-10 18:07:33 +0800

    fix：layout & constant

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,16 @@
  * @Author       : Archer<ahh666@qq.com>
  * @Date         : 2024-04-12 16:54:05
  * @LastEditors  : Archer<ahh666@qq.com>
- * @LastEditTime : 2024-04-18 15:42:28
+ * @LastEditTime : 2024-04-22 16:33:56
  * @FilePath     : \vue3-template-nuxt3\layouts\default.vue
  * @Description  : Description
 -->
 <template>
-  <main class="px-10 py-20 text-center">
+  <main class="px0 py0">
     <Header />
-    <slot />
-    <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
+    <div bg="#f4f4f4" py15px>
+      <slot />
     </div>
+    <Footer />
   </main>
 </template>
--- layouts/home.vue
+++ layouts/home.vue
@@ -2,12 +2,12 @@
  * @Author       : Archer<ahh666@qq.com>
  * @Date         : 2024-04-12 16:54:05
  * @LastEditors  : Archer<ahh666@qq.com>
- * @LastEditTime : 2024-04-18 15:42:35
+ * @LastEditTime : 2024-04-22 16:14:20
  * @FilePath     : \vue3-template-nuxt3\layouts\home.vue
  * @Description  : Description
 -->
 <template>
-  <main class="px-10 py-20 text-center">
+  <main class="px0 py0">
     <Header />
     <slot />
     <Footer />
