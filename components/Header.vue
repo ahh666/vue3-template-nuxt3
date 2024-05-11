218cbbdfc90974e4faf1f1ffb7b1d897a8fd3f19
ahh <ahh666@qq.com>
2024-05-11 16:24:38 +0800

    logo click

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
  * @Author       : Archer<ahh666@qq.com>
  * @Date         : 2024-04-17 16:11:29
  * @LastEditors  : Archer<ahh666@qq.com>
- * @LastEditTime : 2024-05-11 16:22:53
+ * @LastEditTime : 2024-05-11 16:24:08
  * @FilePath     : \vue3-template-nuxt3\components\Header.vue
  * @Description  : Description
 -->
@@ -13,7 +13,7 @@ const router = useRouter()
 <template>
   <header h110px w100vw py10px px flex justify-center>
     <div w1200px>
-      <a inline-block>
+      <a inline-block href="/">
         <img w329px src="/logo.png" alt="logo">
       </a>
       <button
